15d330c65773a2641753aee64e987cdf0ce3d46f
rollo <rollovitch.roll@yandex.ru>
2023-08-21 17:00:13 +0300

    +Components: Divider, Multiplier +Beans: divCalculator, multCalculator

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import com.example.demo.component.calculator.Calculator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
